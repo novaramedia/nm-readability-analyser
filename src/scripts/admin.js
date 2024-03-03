4b0fb29d24c97c19b5614424224ab4d4992dee90
varffs <patrickbest@patrickbest.com>
2024-03-03 15:30:36 +0000

    Add: Classic editor support

--- src/scripts/admin.js
+++ src/scripts/admin.js
@@ -1,7 +1,4 @@
-/**
- * Run scripts on document ready
- * No jQuery here sorry
- */
+/* global tinymce */
 
 import readabilityScores from "readability-scores";
 // Big reference: https://github.com/wooorm/readability/blob/main/src/index.js
@@ -10,31 +7,33 @@ import _ from "lodash";
 const strip = (html) => {
   let doc = new DOMParser().parseFromString(html, "text/html");
   return doc.body.textContent || "";
-}
+};
 
-const replacer = (match, p1, p2, p3) => { // replace with the 3rd matching group which will be the contents inside the Wordpress shortcode
+const replacer = (match, p1, p2, p3) => {
+  // replace with the 3rd matching group which will be the contents inside the Wordpress shortcode
   return p3;
-}
+};
 
 const gradeToAge = (grade) => {
   const age = Math.round(grade + 5); // us grade level to age
   const max = 22;
 
   return age > max ? max : age;
-}
+};
 
 const averageOfArray = (array) => array.reduce((a, b) => a + b) / array.length;
 
-const runAnalysis = () => {
-  const currentPost = wp.data.select("core/editor").getCurrentPost();
-  const postContent = strip(currentPost.content).replace(
+const runAnalysis = (rawContent) => {
+  if (!rawContent) {
+    return;
+  }
+
+  const postContent = strip(rawContent).replace(
     /\[(\w+)([^[\]]*?)\](.*?)\[\/\1\]/g,
     replacer
   );
 
   const bigAnalysis = readabilityScores(postContent);
-
-  console.log(bigAnalysis);
 
   const average = averageOfArray([
     gradeToAge(bigAnalysis.daleChall),
@@ -53,9 +52,12 @@ const runAnalysis = () => {
 
   // readabilityGradeSpan.textContent = labelScore(analysis.score);
 
-  let daleChallDifficultWordCountSpan = document.querySelector("#nm_readability-dale-chall-difficult-word-count");
+  let daleChallDifficultWordCountSpan = document.querySelector(
+    "#nm_readability-dale-chall-difficult-word-count"
+  );
 
-  daleChallDifficultWordCountSpan.textContent = bigAnalysis.daleChallDifficultWordCount;
+  daleChallDifficultWordCountSpan.textContent =
+    bigAnalysis.daleChallDifficultWordCount;
 
   let polysyllabicWordCountSpan = document.querySelector(
     "#nm_readability-polysyllabic-word-count"
@@ -67,8 +69,9 @@ const runAnalysis = () => {
     "#nm_readability-polysyllabic-word-percentage"
   );
 
-  polysyllabicWordPercentageSpan.textContent =
-    Math.round(bigAnalysis.polysyllabicWordCount / bigAnalysis.wordCount * 100);
+  polysyllabicWordPercentageSpan.textContent = Math.round(
+    (bigAnalysis.polysyllabicWordCount / bigAnalysis.wordCount) * 100
+  );
 
   let wordCountSpan = document.querySelector("#nm_readability-word-count");
 
@@ -81,10 +84,22 @@ const runAnalysis = () => {
   sentenceCountSpan.textContent = bigAnalysis.sentenceCount;
 };
 
-const debouncedRunAnalysis = _.debounce(() => {
-  console.log("Running analysis");
-  runAnalysis();
+const debouncedBlockEditor = _.debounce(() => {
+  const currentPost = wp.data.select("core/editor").getCurrentPost();
+
+  runAnalysis(currentPost.content);
 }, 500);
+
+const debouncedClassicChange = _.debounce(() => {
+  let editor = tinymce.get("content");
+
+  if (editor) {
+    const currentPost = editor.getContent();
+
+    runAnalysis(currentPost);
+  }
+}
+, 3000);
 
 document.addEventListener("DOMContentLoaded", () => {
   wp.domReady(() => {
@@ -92,8 +107,26 @@ document.addEventListener("DOMContentLoaded", () => {
       return;
     }
 
-    wp.data.subscribe(() => {
-      debouncedRunAnalysis();
-    });
+    if (!wp.data) {
+      // in this case we will use tinymce. we need to find how to get the content of the editor
+
+      tinymce.on("AddEditor", function () {
+        let editor = tinymce.get("content");
+
+        if (editor) {
+          const currentPost = editor.getContent();
+
+          runAnalysis(currentPost);
+
+          editor.on("change", () => {
+            debouncedClassicChange();
+          });
+        }
+      });
+    } else {
+      wp.data.subscribe(() => {
+        debouncedBlockEditor();
+      });
+    }
   });
 });
